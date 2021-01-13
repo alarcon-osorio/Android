96351de25b70c29bebefd4819a40f2c59b607978
alarcon_osorio <alarcon_osorio@hotmail.com>
2021-01-12 19:33:56 -0500

    Primera App. Close #12

--- Pildoras_Andoid/MyApplication/app/src/main/java/com/example/myapp/MainActivity.java
+++ Pildoras_Andoid/MyApplication/app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contador =0;
+    }
+
+    public void incrementaContador(View vista){
+        contador ++;
+        resultado();
+    }
+
+    public void resetContador(View vista){
+        contador=0;
+        resultado();
+    }
+
+    public void decrementaContador(View vista){
+        contador --;
+        resultado();
+    }
+
+    public void resultado(){
+        TextView texto =(TextView) findViewById(R.id.texto);
+        texto.setText("Contador: "+ contador);
     }
 }
